42880490e1f329508c4898125c6b3a3238f549de
sn_baby <sn_baby@qq.com>
2020-11-01 22:45:34 +0800

    修改

--- src/main/java/com/isoftstone/jxyz/util/Utils.java
+++ src/main/java/com/isoftstone/jxyz/util/Utils.java
@@ -4,6 +4,13 @@ import java.io.File;
 import java.nio.charset.Charset;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.alibaba.fastjson.JSONObject;
+import com.github.drinkjava2.jdbpro.SqlItem;
+import com.github.drinkjava2.jsqlbox.DB;
 
 import net.lingala.zip4j.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
@@ -24,4 +31,117 @@ public class Utils {
 		}
 		zFile.extractAll(dest); // 将文件抽出到解压目录(解压)
 	}
+
+	public static void consStr(List<SqlItem> sqlItemList, JSONObject jsb, String name) {
+		if (jsb == null) {
+			return;
+		}
+		Object obj = jsb.get(name);
+		if (obj == null) {
+			return;
+		}
+
+		if (StringUtils.isBlank(obj.toString())) {
+			return;
+		}
+
+		try {
+			sqlItemList.add(DB.notNull(name + ",", obj.toString()));
+		} catch (Exception e) {
+			// TODO: handle exception
+			// 异常直接丢弃，不存入数据库
+		}
+	}
+
+	public static void consInteger(List<SqlItem> sqlItemList, JSONObject jsb, String name) {
+		if (jsb == null) {
+			return;
+		}
+		Object obj = jsb.get(name);
+		if (obj == null) {
+			return;
+		}
+
+		try {
+			if (StringUtils.isNotBlank(obj.toString())) {
+				sqlItemList.add(DB.notNull(name + ",", Long.parseLong(obj.toString())));
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			// 异常直接丢弃，不存入数据库
+		}
+	}
+
+	public static void consDecimal(List<SqlItem> sqlItemList, JSONObject jsb, String name) {
+		if (jsb == null) {
+			return;
+		}
+		Object obj = jsb.get(name);
+		if (obj == null) {
+			return;
+		}
+		try {
+			sqlItemList.add(DB.notNull(name + ",", Double.parseDouble(obj.toString())));
+		} catch (Exception e) {
+			// TODO: handle exception
+			// 异常直接丢弃，不存入数据库
+		}
+	}
+
+	public static void consDate(List<SqlItem> sqlItemList, JSONObject jsb, String name) {
+		if (jsb == null) {
+			return;
+		}
+		Object obj = jsb.get(name);
+		if (obj == null) {
+			return;
+		}
+		try {
+			sqlItemList.add(DB.notNull(name + ",", df.format(df.parse(obj.toString()))));
+		} catch (Exception e) {
+			// TODO: handle exception
+			// 异常直接丢弃，不存入数据库
+		}
+	}
+
+	public static void traverFile(File file, List<File> fileList) {
+		if (file.isDirectory()) {
+			for (File tempFile : file.listFiles()) {
+				traverFile(tempFile, fileList);
+			}
+		} else {
+			fileList.add(file);
+		}
+	}
+	
+	/**
+	 * 删除文件或文件夹
+	 * @param directory
+	 */
+	public static void delAllFile(File directory){
+	    if (!directory.isDirectory()){
+	        directory.delete();
+	    } else{
+	        File [] files = directory.listFiles();
+
+	        // 空文件夹
+	        if (files.length == 0){
+	            directory.delete();
+	            return;
+	        }
+
+	        // 删除子文件夹和子文件
+	        for (File file : files){
+	            if (file.isDirectory()){
+	                delAllFile(file);
+	            } else {
+	                file.delete();
+	            }
+	        }
+
+	        // 删除文件夹本身
+	        directory.delete();
+	    }
+	}
+	
 }
